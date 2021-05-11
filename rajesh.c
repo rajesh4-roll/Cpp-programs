02b1531fc7838a2538723d86c5ba805746c4eeb1
rajesh_srma <raajesh.srma@gmail.com>
2021-05-11 08:38:56 +0545

    chages added in rajesh.c

--- rajesh.c
+++ rajesh.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-typedef struct name
-{
+#define SQUARE(x) x * x
+typedef struct name{ 
 	char naam[20];
 };
-#define SQUARE(x) x*x
-int sqr(x){
-    return x*x;
-}
+int sqr(int x);
 int main(){
-	name rajes;
-    print("Hello, World! ");
-    scanf("%s",rajes.naam);
-    printf("srq(3+2: ",sqr(3+2));
-    printf("SQUARE(3+2): ",SQUARE(3+2));
+	struct name rajes;
+    printf("Hello, World!\n ");
+    // scanf("%s",rajes.naam);
+    printf("srq(3+2: %d \n ", sqr(3+2));
+    printf("SQUARE(3+2): %d  \n",SQUARE(3+2));
     return 0;
+}
+int sqr(int x){
+	return x * x;
 }
