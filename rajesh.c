d4b567e01f573aac62fcd148e8180cd3badf12fd
rajesh_srma <raajesh.srma@gmail.com>
2021-05-11 11:05:04 +0545

    afd

--- rajesh.c
+++ rajesh.c
@@ -6,10 +6,12 @@ typedef struct name{
 int sqr(int x);
 int main(){
 	struct name rajes;
+	int x = 5;
     printf("Hello, World!\n ");
     // scanf("%s",rajes.naam);
     printf("srq(3+2: %d \n ", sqr(3+2));
     printf("SQUARE(3+2): %d  \n",SQUARE(3+2));
+    cout << ++x + x++;
     return 0;
 }
 int sqr(int x){
